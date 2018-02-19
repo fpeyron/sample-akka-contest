6e8a254f63c8dd1c4b3e8bf9d1a9f0e35269017a
Florent Peyron <florent.peyron@ext.betc.com>
2018-02-19 09:28:28 +0100

    Remove password in swagger description

--- src/main/scala/com/betc/danon/game/routes/SwaggerRoute.scala
+++ src/main/scala/com/betc/danon/game/routes/SwaggerRoute.scala
@@ -13,7 +13,7 @@ trait SwaggerRoute extends Directives with SwaggerGenerator with CorsSupport {
   import SwaggerHttpService._
 
   override val apiClasses = Set(classOf[BoPrizeRoute], classOf[BoGameRoute], classOf[PartnerRoute])
-  override val info = Info(version = "1.0", description = "3 users : \n- admin_fr / p4ssw0rd\n- admin_ca / p4ssw0rd\n- admin_jp / p4ssw0rd")
+  override val info = Info(version = "1.0", description = "") //"3 users : \n- admin_fr / p4ssw0rd\n- admin_ca / p4ssw0rd\n- admin_jp / p4ssw0rd")
   override val externalDocs = Some(new ExternalDocs("Core Docs", "http://acme.com/docs"))
   override val securitySchemeDefinitions = Map("basicAuth" -> new BasicAuthDefinition())
   override val unwantedDefinitions = Seq("Function1", "Function1RequestContextFutureRouteResult")
